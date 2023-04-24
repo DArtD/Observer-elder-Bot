d7e8ab8681178f5be32037b32a32b33e034aaaf7
DArtD <114067414+DArtD@users.noreply.github.com>
2023-04-24 23:18:39 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-group_chat = os.getenv("GROUP_CHAT")
-BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
-admin = os.getenv("ADMIN_ID")
-OPEN_WEATHER_TOKEN = str(os.getenv("OPEN_WEATHER_TOKEN"))
+group_chat = "-1001844038287"
+BOT_TOKEN = '6008950844:AAEzzRDQO12uDLN74hfMbSZDNE6qRBJ2Lbc'
+admin = '1982872796'
+OPEN_WEATHER_TOKEN = "d7d94cd3ac0212c52fc2f6af7569d737"
 
 ip = os.getenv("ip")
 
